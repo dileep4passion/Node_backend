3d85781bb2923d03d0acbe896cfb5fdee20df3fa
Sai Dileep <64729076+dileep4passion@users.noreply.github.com>
2025-03-11 08:44:36 -0500

    Revert "env"
    This reverts commit a483a484f82ed5377aa924f6364d21fa2c2f5729.

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,10 +1,9 @@
 import express from "express";
-import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT;
+const port = 6969;
 
-// app.get("/", (req, res) => {
+// app.get("/", (req, res) => { 
 //   res.send("Hello from Jarvis");
 // });
 
@@ -31,44 +30,44 @@ app.post("/login", (req, res) => {
 });
 
 //retrieving users
-app.get("/users", (req, res) => {
-  res.status(200).send(data);
-});
+app.get("/users",(req,res)=>{
+    res.status(200).send(data)
+})
 
 //retrieving users by id
-app.get("/users/:id", (req, res) => {
-  const user = data.find((user) => user.id === parseInt(req.params.id));
-  if (!user) {
-    return res.status(404).send("user not found");
-  } else {
-    res.status(200).send(user);
-  }
-});
+app.get("/users/:id",(req,res)=>{
+    const user = data.find(user=>user.id===parseInt(req.params.id))
+    if (!user) {
+        return res.status(404).send("user not found")
+    }else {
+        res.status(200).send(user)
+    }
+    
+})
 
 //update user
-app.put("/update/:id", (req, res) => {
-  const user = data.find((user) => user.id === parseInt(req.params.id));
-  if (!user) {
-    return res.status(404).send("user not found");
-  } else {
-    const { name, age } = req.body;
-    (user.name = name), (user.age = age);
-  }
-  return res.status(200).send(user);
-});
+app.put("/update/:id",(req,res)=>{
+    const user = data.find(user=>user.id === parseInt(req.params.id))
+    if (!user) {
+        return res.status(404).send("user not found")
+    }else {
+        const {name,age} = req.body
+        user.name = name,
+        user.age = age
+    }
+    return res.status(200).send(user);
+})
 
 //delete user
-app.delete("/delete/:id", (req, res) => {
-  const userIndex = data.findIndex(
-    (user) => user.id === parseInt(req.params.id)
-  );
-  if (userIndex == -1) {
-    return res.status(404).send("user not found");
-  } else {
-    data.splice(userIndex, 1);
-    return res.status(204).send(`deleted`);
-  }
-});
+app.delete("/delete/:id",(req,res)=>{
+    const userIndex = data.findIndex(user=>user.id === parseInt(req.params.id))
+    if (userIndex==-1) {
+        return res.status(404).send("user not found")
+    }else {
+        data.splice(userIndex,1)
+        return res.status(204).send(`deleted`)
+    }
+})
 
 //server code
 app.listen(port, () => {
