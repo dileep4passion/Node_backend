dc23dada058cffa9559d335e4446459844cb8af9
Sai Dileep <64729076+dileep4passion@users.noreply.github.com>
2025-03-12 09:06:54 -0500

    advance logger

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,7 +1,26 @@
 import express from "express";
+import logger from "./utilities.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 6969;
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 // app.get("/", (req, res) => { 
 //   res.send("Hello from Jarvis");
